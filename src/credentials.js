be95795de5691e80db193d9128f712673facc5d3
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2019-02-03 21:57:58 -0500

    Remove my credentials from github

--- src/credentials.js
+++ src/credentials.js
@@ -1,17 +1,16 @@
 // Note to developers.
-// Get credentials at https://developers.google.com/calendar/quickstart/nodejs.
-// Please do not use my credentials object.
+// Please get credentials at https://developers.google.com/calendar/quickstart/nodejs.
 
 const credentials = {
   installed: {
     client_id:
-      "206370061322-eggpdud9ddcj5vmbkmv6slbmlrikob0i.apps.googleusercontent.com",
-    project_id: "calen-1544968010649",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://www.googleapis.com/oauth2/v3/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_secret: "9_AKL0GcqGLQdtr6pK43uT4C",
-    redirect_uris: ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"]
+      "",
+    project_id: "",
+    auth_uri: "",
+    token_uri: "",
+    auth_provider_x509_cert_url: "",
+    client_secret: "",
+    redirect_uris: []
   }
 };
 
